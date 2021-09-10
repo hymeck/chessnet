6dd94a8514c5db2e19185beee204db0b5af205ac
Vadim Yaren <noonimf@gmail.com>
2021-09-11 01:32:15 +0300

    added square value converter

---
+++ src/ChessNet/Converters/SquareValueConverter.cs
@@ -0,0 +1,15 @@
+﻿namespace ChessNet.Converters
+{
+    public class SquareValueConverter
+    {
+        // https://stackoverflow.com/questions/11040646/faster-modulus-in-c-c
+        public (int x, int y) ToCartesianPosition(int square) =>
+            // (square % 8, square / 8)
+            (square & 7, square >> 3);
+
+        public int To1DPosition(int x, int y) => (y << 3) + x;
+
+        public int To1DPosition((int x, int y) cartesianSquare) => 
+            To1DPosition(cartesianSquare.x, cartesianSquare.y);
+    }
+}
---
+++ tests/ChessNet.Tests/SquareValueConverterTests.cs
@@ -0,0 +1,32 @@
+﻿using ChessNet.Converters;
+using Shouldly;
+using Xunit;
+
+namespace ChessNet.Tests
+{
+    public class SquareValueConverterTests
+    {
+        [Theory]
+        [InlineData(0, 0, 0)]
+        [InlineData(15, 7, 1)]
+        [InlineData(20, 4, 2)]
+        public void ToCartesianPosition(int square, int expectedX, int expectedY)
+        {
+            var squareValueConverter = new SquareValueConverter();
+            var (x, y) = squareValueConverter.ToCartesianPosition(square);
+            x.ShouldBe(expectedX);
+            y.ShouldBe(expectedY);
+        }
+        
+        [Theory]
+        [InlineData(7, 1, 15)]
+        [InlineData(4, 2, 20)]
+        [InlineData(0, 0, 0)]
+        public void To1DPosition(int x, int y, int expected)
+        {
+            var squareValueConverter = new SquareValueConverter();
+            var actual = squareValueConverter.To1DPosition((x, y)); // pass tuple for coverage
+            actual.ShouldBe(expected);
+        }
+    }
+}

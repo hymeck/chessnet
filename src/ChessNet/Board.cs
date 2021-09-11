ca5bc7b215f71fd87a8d9389c4461b5c0acd85ef
Vadim Yaren <noonimf@gmail.com>
2021-09-11 12:48:25 +0300

    added square range checking

---
+++ src/ChessNet/Board.cs
@@ -0,0 +1,8 @@
+﻿namespace ChessNet
+{
+    public class Board
+    {
+        public bool IsOnBoard(Square square) => (int) square is 0 and <= 63;
+        public bool IsEmptySquare(Square square) => !IsOnBoard(square);
+    }
+}
---
+++ tests/ChessNet.Tests/BoardTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Shouldly;
+using Xunit;
+
+namespace ChessNet.Tests
+{
+    public class BoardTests
+    {
+        public void IsOn()
+        {
+            var board = new Board();
+            
+            var result = board.IsOnBoard(Square.A1);
+            result.ShouldBe(true);
+            
+            result = board.IsOnBoard(Square.Empty);
+            result.ShouldBe(false);
+            
+            result = board.IsOnBoard((Square)(-11));
+            result.ShouldBe(false);
+            
+            result = board.IsOnBoard((Square)100);
+            result.ShouldBe(false);
+        }
+        
+        [Fact]
+        public void IsEmptySquare()
+        {
+            var board = new Board();
+            var result = board.IsEmptySquare(Square.Empty);
+            result.ShouldBe(true);
+
+            result = board.IsEmptySquare((Square) (-13));
+            result.ShouldBe(true);
+            
+            result = board.IsEmptySquare((Square) (100));
+            result.ShouldBe(true);
+        }
+    }
+}
